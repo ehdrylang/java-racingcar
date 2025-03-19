e7ada9c7467b983fe4579444f9f88ec1046b4c45
east.bridge <east.bridge@kakaocorp.com>
2025-03-19 19:30:12 +0900

    refactor: 안 쓰이는 메서드 삭제

--- src/main/java/game/Cars.java
+++ src/main/java/game/Cars.java
@@ -5,8 +5,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import static view.ResultView.WINNERS_DELIMITER;
-
 public class Cars {
     private final List<Car> cars;
 
@@ -50,18 +48,5 @@ public class Cars {
                 .map(Car::position)
                 .max(Integer::compareTo)
                 .orElseThrow(() -> new IllegalArgumentException("우승자 위치를 찾을 수 없습니다."));
-    }
-
-    public static void printResult(Cars cars) {
-        Map<String, Integer> positionByName = cars.positionByName();
-        int maxPosition = positionByName.values().stream()
-                .max(Integer::compareTo)
-                .orElseThrow(IllegalArgumentException::new);
-        String winners = positionByName.entrySet()
-                .stream()
-                .filter(entry -> entry.getValue() == maxPosition)
-                .map(Map.Entry::getKey)
-                .collect(Collectors.joining(WINNERS_DELIMITER));
-        System.out.println(winners + "가 최종 우승했습니다.");
     }
 }
